372a88d077020d876072aa5d585484a9a463c660
Sean F <77561595+scfitze@users.noreply.github.com>
2021-02-09 19:09:40 -0700

    added factorial
    added code to factorial function

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -57,7 +57,14 @@ std::vector<int> SquaresUntil(int n) {
 int NthFibonacci(int n);
 
 // takes an int, n, and returns the factorial of that int (n!)
-int Factorial(int n);
+int Factorial(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return n * Factorial(n-1);
+    }
+}
 
 // returns -1 if the number is negative and 1 if positive
 int Sign(int num) {
